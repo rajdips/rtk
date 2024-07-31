beecd79afe3998bb60f865ade694a7ab5f02d905
rajdip <34982484+rajdips@users.noreply.github.com>
2024-07-31 06:45:03 +0000

    code commited

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div className="container is-fluid">
       <button onClick={() => handleResetClick()} className="button is-danger">
-        Reset Both Playlists
+        Reset Both Playlists**
       </button>
       <hr />
       <MoviePlaylist />
